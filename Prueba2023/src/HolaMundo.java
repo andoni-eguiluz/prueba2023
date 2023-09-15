acec2e6b66ad497cbffa10557c0f87f0debe083e
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2023-09-15 11:06:23 +0200

    Sigo con el hola mundo, error pendiente

--- Prueba2023/src/HolaMundo.java
+++ Prueba2023/src/HolaMundo.java
@@ -2,5 +2,75 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println( "hola" );
+		// edito
+		// edito
+		// edito
+//		 edito
+//		 edito
+//		 edito
+//		 edito
+//		 edito
+//		 edito
+//		 edito
+//		 TODO corregir esto
+		// FIXME urgente
+//		 if (ss)
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
